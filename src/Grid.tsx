d57b35a97aac4f4bf287c4c32bd386ecb2f49607
Elbert Olivar <elbert.olivar@brightserve.com>
2023-09-21 14:47:34 +0800

    Task #1: Add title and replace tab title

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,32 +1,44 @@
+import { useEffect } from "react";
 import { AgGridReact } from "ag-grid-react";
 import { ColDef } from "ag-grid-community";
 import data from "./near-earth-asteroids.json";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 
+import styles from "./Grid.module.css";
+
 const columnDefs: ColDef[] = [
-  { field: "designation", headerName: "Designation" },
-  { field: "discovery_date", headerName: "Discovery Date" },
-  { field: "h_mag", headerName: "H (mag)" },
-  { field: "moid_au", headerName: "MOID (au)" },
-  { field: "q_au_1", headerName: "q (au)" },
-  { field: "q_au_2", headerName: "Q (au)" },
-  { field: "period_yr", headerName: "Period (yr)" },
-  { field: "i_deg", headerName: "Inclination (deg)" },
-  { field: "pha", headerName: "Potentially Hazardous" },
-  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true, },
+    { field: "designation", headerName: "Designation" },
+    { field: "discovery_date", headerName: "Discovery Date" },
+    { field: "h_mag", headerName: "H (mag)" },
+    { field: "moid_au", headerName: "MOID (au)" },
+    { field: "q_au_1", headerName: "q (au)" },
+    { field: "q_au_2", headerName: "Q (au)" },
+    { field: "period_yr", headerName: "Period (yr)" },
+    { field: "i_deg", headerName: "Inclination (deg)" },
+    { field: "pha", headerName: "Potentially Hazardous" },
+    { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true },
 ];
 
 const NeoGrid = (): JSX.Element => {
-  return (
-    <div className="ag-theme-alpine" style={{ height: 900, width: 1920 }}>
-      <AgGridReact
-        rowData={data}
-        columnDefs={columnDefs}
-        rowGroupPanelShow={'always'}
-      />
-    </div>
-  );
+    const title = "Near-Earth Object Overview";
+
+    useEffect(() => {
+        document.title = title;
+    }, []);
+
+    return (
+        <div className="ag-theme-alpine" style={{ height: 900, width: 1920 }}>
+            <div className={styles.header}>
+                <h1>{title}</h1>
+            </div>
+            <AgGridReact
+                rowData={data}
+                columnDefs={columnDefs}
+                rowGroupPanelShow={"always"}
+            />
+        </div>
+    );
 };
 
 export default NeoGrid;
